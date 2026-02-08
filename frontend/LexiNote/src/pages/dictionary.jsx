5e05796c1d15495efaf984992843483ae2b61b58
Piyush Varman <varmanpiyush@gmail.com>
2026-02-08 14:49:37 +0530

    check for diff systems

--- frontend/LexiNote/src/pages/dictionary.jsx
+++ frontend/LexiNote/src/pages/dictionary.jsx
@@ -4,7 +4,7 @@ export default function Dictionary(){
     return(
         <>
             <main>
-                <h1>This is where the dictionary should go</h1>
+                <h1>The dictionary goes here, right?</h1>
             </main>
         </>
     );
