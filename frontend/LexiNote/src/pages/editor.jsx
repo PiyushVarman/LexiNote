8f078b32584fb74d07ea4e5e1add7d42b35dd95e
Harshne <harshne0501@gmail.com>
2026-02-12 13:04:12 +0530

    hv

--- frontend/LexiNote/src/pages/editor.jsx
+++ frontend/LexiNote/src/pages/editor.jsx
@@ -5,7 +5,7 @@ export default function Editor(){
         <>
             <main>
                 <h1>Rithanya</h1>
-                <h1>Rithanya</h1>
+                <h1>Harshne</h1>
             </main>
         </>
     );
