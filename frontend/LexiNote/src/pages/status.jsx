b677974076ce3e5ac6e1f7ce727a019129802ba7
Piyush Varman <varmanpiyush@gmail.com>
2026-02-19 23:51:37 +0530

    Updated frontend designs and status page

--- frontend/LexiNote/src/pages/status.jsx
+++ frontend/LexiNote/src/pages/status.jsx
@@ -4,8 +4,15 @@ import './status.css'
 export default function Status(){
     return(
         <>
-            <main>
+            <main className="status">
                 <h1>Status</h1>
+                <p>Built on the <strong>MERN</strong> Stack</p>
+                <div className="text">
+                    <p><strong><br/>MongoDB:</strong> Contains more than 2,00,000 words with definitions, parts of speech and synonyms, all hosted online.</p>
+                    <p><strong>Express.js: </strong>Acts as the connection between the frontend and the backend</p>
+                    <p><strong>React.js: </strong>Versatile tool used for frontend design and functionality</p>
+                    <p><strong>Node.js: </strong>Provides server-side runtime environment</p>
+                </div>
             </main>
         </>
     );
