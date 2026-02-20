108f0cb08a01d610df9917010ebefc465df1f6ce
Piyush Varman <varmanpiyush@gmail.com>
2026-02-20 15:30:11 +0530

    Adding online database capabilities via Render

--- frontend/LexiNote/src/pages/dictionary.jsx
+++ frontend/LexiNote/src/pages/dictionary.jsx
@@ -11,7 +11,7 @@ export default function Dictionary() {
     if (!word.trim()) return;
 
     try {
-      const response = await fetch(`http://localhost:5000/word/${word}`);
+      const response = await fetch(`https://lexinote.onrender.com/word/${word}`);
       console.log("Response status:", response.status);
       console.log(results);
       if (!response.ok) {
