1366b338426a6b3f06cde24db43e75ddd52c9e5c
Piyush Varman <varmanpiyush@gmail.com>
2026-02-28 23:22:36 +0530

    fix: puppeteer

--- backend/server.js
+++ backend/server.js
@@ -120,7 +120,7 @@ app.post("/save", async (req, res) => {
     `;
 
     const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: ["--no-sandbox", "--disable-setuid-sandbox","--disable-dev-shm-usage"],
     });
 
     const page = await browser.newPage();
